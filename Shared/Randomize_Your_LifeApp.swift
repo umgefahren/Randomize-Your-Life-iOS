51cdea3159ff6728f159966ea71d98eafd6d9d02
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-06-19 11:31:52 +0200

    Initial Commit

---
+++ Shared/Randomize_Your_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Randomize_Your_LifeApp.swift
+//  Shared
+//
+//  Created by Hannes Furmans on 19.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Randomize_Your_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
